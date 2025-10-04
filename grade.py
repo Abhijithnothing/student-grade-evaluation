33b151619d6970857357b63497cb99f893a01b25
Abhijithnothing <a00931668@gmail.com>
2025-10-04 08:28:31 +0530

    check grade

---
+++ grade.py
@@ -0,0 +1,16 @@
+a=int(input("Enter the marks: "))
+if a>=90:
+    print("Grade A+")
+elif a>=80:
+    print("Grade A")            
+elif a>=70:
+    print("Grade B")    
+elif a>=60:
+    print("Grade C")
+elif a>=50:
+    print("Grade D")
+elif a>=30:
+    print("Grade E")
+else:
+    print("Grade F")
+    
